34da392df36ba86a1f35e2a234201c283ae48d43
psh0121 <tngus2057@gmail.com>
2022-10-02 00:11:31 +0900

    create file BaekJoon_3003.java

---
+++ BaekJoon/Step/IO_And_Arithmetic_Operations/IO_And_Arithmetic_Operations/src/BaekJoon_3003.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class BaekJoon_3003 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int k = 1 - (scan.nextInt());
+		int q = 1 - (scan.nextInt());
+		int l = 2 - (scan.nextInt());
+		int bs = 2 - (scan.nextInt());
+		int n = 2 - (scan.nextInt());
+		int p = 8 - (scan.nextInt());
+		
+		System.out.println(k + " " + q + " " + l + " " + bs + " " + n + " " + p);
+
+	}
+
+}
