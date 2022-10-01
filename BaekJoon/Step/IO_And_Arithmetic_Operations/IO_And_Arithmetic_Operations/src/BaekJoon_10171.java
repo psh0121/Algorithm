06dd34402e97b6aaa942cc9fe6fdf2169cc6a627
psh0121 <tngus2057@gmail.com>
2022-10-02 00:45:02 +0900

    create file BaekJoon_10171.java

---
+++ BaekJoon/Step/IO_And_Arithmetic_Operations/IO_And_Arithmetic_Operations/src/BaekJoon_10171.java
@@ -0,0 +1,12 @@
+
+public class BaekJoon_10171 {
+
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+
+	}
+
+}
