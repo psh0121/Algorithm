fa53c06c038f9093803514e7767243f15a10cd5d
psh0121 <tngus2057@gmail.com>
2022-10-02 00:02:06 +0900

    create file BaekJoon_18108.java

---
+++ BaekJoon/Step/IO_And_Arithmetic_Operations/IO_And_Arithmetic_Operations/src/BaekJoon_18108.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class BaekJoon_18108 {
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int value = scan.nextInt();
+		
+		System.out.println(value - 543);
+	}
+
+}
