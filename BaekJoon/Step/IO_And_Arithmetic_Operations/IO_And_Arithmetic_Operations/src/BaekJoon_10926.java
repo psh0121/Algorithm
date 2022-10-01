0208fd1d86a48814ad1f6e72b5740706775e1262
psh0121 <tngus2057@gmail.com>
2022-10-02 00:00:33 +0900

    create file BaekJoon_10926.java

---
+++ BaekJoon/Step/IO_And_Arithmetic_Operations/IO_And_Arithmetic_Operations/src/BaekJoon_10926.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class BaekJoon_10926 {
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String value = scan.next();
+		
+		System.out.println(value + "??!");
+	}
+
+}
