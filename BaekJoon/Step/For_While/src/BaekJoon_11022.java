4ade9ffd9ae011164fb7e87344e887fc14dfcf4f
psh0121 <tngus2057@gmail.com>
2022-10-03 14:10:37 +0900

    create file BaekJoon_11022.java

---
+++ BaekJoon/Step/For_While/src/BaekJoon_11022.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class BaekJoon_11022 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int cnt = scan.nextInt();
+		
+		int sNum = 1; // case시작넘버 
+		
+		int A;
+		int B;
+		
+		for(int i = 0; i < cnt; i++)
+		{
+			A = scan.nextInt();
+			B = scan.nextInt();
+			
+			System.out.println("Case #" + sNum + ": " + A + " + " + B + " = " + (A+B));
+			sNum++;
+		}
+
+	}
+
+}
