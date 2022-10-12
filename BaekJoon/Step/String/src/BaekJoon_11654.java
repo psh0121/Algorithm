407cd647c3c1de09be1669a03a3f70062519a490
psh0121 <tngus2057@gmail.com>
2022-10-12 21:23:17 +0900

    create file BaekJoon_11654.java

---
+++ BaekJoon/Step/String/src/BaekJoon_11654.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class BaekJoon_11654 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		String value = scan.next();
+		int result = (int)value.charAt(0);
+		
+		System.out.println(result);
+	}
+
+}
